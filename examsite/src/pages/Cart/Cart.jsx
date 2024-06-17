7b7215c12e4ebdbb6a8f806f557e142b90035039
IronPavlo <povilas.kasko@proton.me>
2024-06-17 20:53:19 +0300

    Rudimentary Cart made/ checkout to be made

--- examsite/src/pages/Cart/Cart.jsx
+++ examsite/src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect } from 'react'
 import Header from '../../components/Header/Header';
 import { useParams } from 'react-router-dom';
+import "./Cart.css"
 
 function Cart(props) {
 
@@ -13,8 +14,33 @@ console.log(cartParsed)
   return (
     <>
     <Header/>
-    
+    <div className='cartWrapper'>
+      <div className='cartContentWrapper'>
+          <div className='cartItem'>
+            <div className='cartItemExtra'>
+              <div className='cartItemImageWrapper'>Cart</div>
+              <div className='cartItemDetails'>Contents</div>
+            </div>
+            <div className='cartItemPrice'>Price</div>
+          </div>
 
+        {cartParsed.map((product,index)=>{
+          return  <div key={index} className='cartItem'>
+                    <div className='cartItemImageWrapper'>imgplaceholder</div>
+                    <div className='cartItemDetails'>
+                      <h3>{product.name}</h3>
+                      <h5>Size: {product.size.toUpperCase()}</h5>
+                    </div>
+                    <div className='cartItemQuantity'>
+                      x{product.quantity}
+                    </div>
+                    <div className='cartItemPrice'>
+                    € {product.price * product.quantity}
+                    </div>
+                  </div>
+        })}
+      </div>
+    </div> 
     </>
   )
 }
