ee06e746daac5336455628406ec30c1637c2fe79
IronPavlo <povilas.kasko@proton.me>
2024-06-18 18:50:58 +0300

    Cart Counter + Navbar rerenders every 3 sec

--- examsite/src/components/Navbar/Navbar.jsx
+++ examsite/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css"
+import { useState } from "react";
 
 function Navbar() {
+  let cartSize="";
+  const cart=JSON.parse(localStorage.getItem('cart'))
+  const[updateRenderAmount,setUpdate]=useState(0)
+    setTimeout(()=>{
+        setUpdate(updateRenderAmount+1)
+    },3000)
+  
 
-
-    
-
+    if(cart!=null && cart.length!=0){
+      cartSize=String(cart.length)
+    }
     return (
       <>
         <ul className="navbar">
@@ -14,7 +22,7 @@ function Navbar() {
               <Link to={"/shop"}><li>Shop</li></Link>
             </div>
             <div>
-              <Link to={"/cart"} className="cart"><li>Cart</li></Link>
+              <Link to={"/cart"} className="cart"><li>{cartSize} Cart</li></Link>
             </div>
         </ul>
         
